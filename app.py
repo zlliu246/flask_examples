3070544d7aa6bf3fda9177ddf50e11d98302a037
liuzuolin1996 <liuzuolin1996@gmail.com>
2022-01-30 18:32:10 +0800

    added 2 pages

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello world"
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run()
